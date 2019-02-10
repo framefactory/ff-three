5a697415786073c03120615f33561550482fb456
Ralph Wiedemeier <ralph@framefactory.ch>
2019-02-10 21:11:47 +0100

    sprite/group refactored

--- source/HTMLSprite.ts
+++ source/HTMLSprite.ts
@@ -7,9 +7,6 @@
 
 import * as THREE from "three";
 
-import { html } from "@ff/ui/CustomElement";
-import Viewport from "./Viewport";
-
 ////////////////////////////////////////////////////////////////////////////////
 
 const _vec3a = new THREE.Vector3();
@@ -19,12 +16,13 @@ const _vec3d = new THREE.Vector3();
 const _vec2a = new THREE.Vector2();
 const _vec2b = new THREE.Vector2();
 
-export { Viewport, html };
-
 export enum EQuadrant { TopRight, TopLeft, BottomLeft, BottomRight }
 
-
-export default class HTMLSprite extends THREE.Mesh
+/**
+ * A Three.js Object representing a 3D renderable part and a 2D (HTML) part.
+ * HTML sprites should have a [[HTMLSpriteGroup]] as their parent.
+ */
+export default class HTMLSprite extends THREE.Object3D
 {
     readonly isHTMLSprite = true;
 
@@ -32,27 +30,86 @@ export default class HTMLSprite extends THREE.Mesh
     orientationAngle = 0;
     orientationQuadrant: EQuadrant = EQuadrant.TopLeft;
 
+    private _elements = new Map<HTMLElement, HTMLElement>();
+    private _visible = true;
+
     constructor()
     {
         super();
         this.frustumCulled = false;
     }
 
+    get visible() {
+        return this._visible;
+    }
+    set visible(visible: boolean) {
+        if (visible !== this._visible && this._elements) {
+            this._visible = visible;
+            this._elements.forEach(element => {
+                if (element) {
+                    this.setHTMLElementVisible(element, visible)
+                }
+            });
+        }
+    }
+
+    dispose()
+    {
+        this._elements.forEach((element, container) => {
+            if (element) {
+                container.removeChild(element);
+            }
+        });
+
+        this._elements.clear();
+    }
+
+    disposeHTMLElement(container: HTMLElement)
+    {
+        const element = this._elements.get(container);
+
+        if (element) {
+            this._elements.delete(container);
+            container.removeChild(element);
+        }
+    }
+
+    /**
+     * Called when the 3D parts of the sprite should be updated because
+     * the underlying data has been changed.
+     */
     update()
     {
+        this._elements.forEach(element => {
+            if (element) {
+                this.updateHTMLElement(element)
+            }
+        });
     }
 
-    updateHTMLElement(element: HTMLElement, viewport: Viewport)
+    /**
+     * Called when the model-view of the sprite has changed.
+     * This updates the position and orientation of the HTML element.
+     * @param container
+     * @param anchor
+     * @param camera
+     */
+    renderHTMLElement(container: HTMLElement, camera: THREE.Camera, anchor?: THREE.Object3D): HTMLElement
     {
-    }
+        anchor = anchor || this;
 
-    renderHTMLElement(element: HTMLElement, viewport: Viewport, camera: THREE.Camera)
-    {
+        let element = this._elements.get(container);
+        if (!element) {
+            element = this.createHTMLElement();
+            container.appendChild(element);
+            this._elements.set(container, element);
+        }
+
         _vec3a.set(0, 0, 0);
-        _vec3a.applyMatrix4(this.modelViewMatrix);
+        _vec3a.applyMatrix4(anchor.modelViewMatrix);
 
         _vec3b.set(0, 1, 0);
-        _vec3b.applyMatrix4(this.modelViewMatrix);
+        _vec3b.applyMatrix4(anchor.modelViewMatrix);
 
         _vec3c.copy(_vec3b).sub(_vec3a).normalize();
         _vec3d.set(0, 0, 1);
@@ -66,21 +123,49 @@ export default class HTMLSprite extends THREE.Mesh
         _vec2a.set(_vec3a.x, _vec3a.y);
         _vec2b.sub(_vec2a);
 
-        const x = (_vec3b.x + 1) * 0.5 * viewport.width;
-        const y = (1 - _vec3b.y) * 0.5 * viewport.height;
+        const x = (_vec3b.x + 1) * 0.5 * container.clientWidth;
+        const y = (1 - _vec3b.y) * 0.5 * container.clientHeight;
 
         element.style.left = x.toString() + "px";
         element.style.top = y.toString() + "px";
 
         const angle = this.orientationAngle = _vec2b.angle();
         this.orientationQuadrant = Math.floor(2 * angle / Math.PI);
+
+        return element;
     }
 
-    createHTMLElement(): HTMLElement
+    /**
+     * Called when the sprite becomes visible in a viewport.
+     * Returns a HTML element to visualize the 2D part of the sprite in the viewport.
+     */
+    protected createHTMLElement(): HTMLElement
     {
         const element = document.createElement("div");
         element.innerText = "HTML Sprite";
         element.classList.add("ff-html-sprite");
+
         return element;
     }
+
+    /**
+     * Called when the HTML parts of the sprite should be updated because
+     * the underlying data has been changed. This is called once for each viewport
+     * the sprite is represented in with a HTML element.
+     * @param element The HTML element that should be updated.
+     */
+    protected updateHTMLElement(element: HTMLElement)
+    {
+    }
+
+    /**
+     * Called when the visibility of the sprite changes.
+     * @param element The HTML element whose visibility should be changed.
+     * @param visible true if the sprite is currently visible.
+     */
+    protected setHTMLElementVisible(element: HTMLElement, visible: boolean)
+    {
+        element.style.display = visible ? "block" : "none";
+    }
+
 }
--- source/HTMLSpriteGroup.ts
+++ source/HTMLSpriteGroup.ts
@@ -7,9 +7,6 @@
 
 import * as THREE from "three";
 
-import { Dictionary } from "@ff/core/types";
-
-import Viewport, { IViewportDisposeEvent } from "./Viewport";
 import HTMLSprite from "./HTMLSprite";
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -21,135 +18,55 @@ export default class HTMLSpriteGroup extends THREE.Object3D
 {
     readonly isHTMLSpriteGroup = true;
 
-    protected viewports = new Map<Viewport, Dictionary<HTMLElement>>();
     private _visible = true;
 
     get visible() {
         return this._visible;
     }
     set visible(visible: boolean) {
-        if (visible !== this._visible && this.viewports) {
+        if (visible !== this._visible) {
             this._visible = visible;
 
-            const children = this.children;
-            this.viewports.forEach((elements, viewport) => {
-                for (let i = 0, n = children.length; i < n; ++i) {
-                    const sprite = children[i] as HTMLSprite;
-                    const element = elements[sprite.uuid] as HTMLElement;
-                    if (element) {
-                        element.style.display = visible ? "block" : "none";
-                    }
-                }
-            });
+            const children = this.children as HTMLSprite[];
+            for (let i = 0, n = children.length; i < n; ++i) {
+                children[i].visible = visible;
+            }
         }
     }
 
     dispose()
     {
-        const children = this.children;
-        this.viewports.forEach((elements, viewport) => {
-            for (let i = 0, n = children.length; i < n; ++i) {
-                const sprite = children[i] as HTMLSprite;
-                const element = elements[sprite.uuid] as HTMLElement;
-                if (element) {
-                    viewport.overlay.removeChild(element);
-                }
-            }
-        })
+        const children = this.children as HTMLSprite[];
+        for (let i = 0, n = children.length; i < n; ++i) {
+            children[i].dispose();
+        }
     }
 
-    render(viewport: Viewport, camera: THREE.Camera)
+    disposeHTMLContainer(container: HTMLElement)
     {
-        if (!viewport.overlay) {
-            console.warn("viewport missing overlay element");
-            return;
+        const children = this.children as HTMLSprite[];
+        for (let i = 0, n = children.length; i < n; ++i) {
+            children[i].disposeHTMLElement(container);
         }
+    }
 
+    render(container: HTMLElement, camera: THREE.Camera)
+    {
         if (!this.visible) {
             return;
         }
 
-        const elements = this.viewports.get(viewport) || this.registerViewport(viewport);
-        const children = this.children;
-
+        const children = this.children as HTMLSprite[];
         for (let i = 0, n = children.length; i < n; ++i) {
-            const sprite = children[i] as HTMLSprite;
-            const element = elements[sprite.uuid] as HTMLElement;
-            if (element) {
-                sprite.renderHTMLElement(element, viewport, camera);
-            }
+            children[i].renderHTMLElement(container, camera);
         }
     }
 
-    update(sprite: HTMLSprite)
+    update()
     {
-        sprite.update();
-
-        this.viewports.forEach((elements, viewport) => {
-            const element = elements[sprite.uuid];
-            if (element) {
-                sprite.updateHTMLElement(element, viewport);
-            }
-        });
-    }
-
-    add(sprite: HTMLSprite)
-    {
-        this.viewports.forEach((elements, viewport) => {
-            const element = sprite.createHTMLElement();
-            if (element) {
-                elements[sprite.uuid] = element;
-                viewport.overlay.appendChild(element);
-            }
-        });
-
-        return super.add(sprite);
-    }
-
-    remove(sprite: HTMLSprite)
-    {
-        this.viewports.forEach((elements, viewport) => {
-            const element = elements[sprite.uuid];
-            if (element) {
-                viewport.overlay.removeChild(element);
-                elements[sprite.uuid] = undefined;
-            }
-        });
-
-        return super.remove(sprite);
-    }
-
-    protected registerViewport(viewport: Viewport): Dictionary<HTMLElement>
-    {
-        const elements = {};
-        this.viewports.set(viewport, elements);
-
-        const children = this.children;
+        const children = this.children as HTMLSprite[];
         for (let i = 0, n = children.length; i < n; ++i) {
-            const sprite = children[i] as HTMLSprite;
-            const element = sprite.createHTMLElement();
-            if (element) {
-                elements[sprite.uuid] = element;
-                viewport.overlay.appendChild(element);
-            }
+            children[i].update();
         }
-
-        viewport.on<IViewportDisposeEvent>("dispose", this.onViewportDispose, this);
-
-        return elements;
-    }
-
-    protected onViewportDispose(event: IViewportDisposeEvent)
-    {
-        const overlay = event.viewport.overlay;
-        const elements  = this.viewports.get(event.viewport);
-        const children = this.children;
-
-        for (let i = 0, n = children.length; i < n; ++i) {
-            const sprite = children[i] as HTMLSprite;
-            overlay.removeChild(elements[sprite.uuid]);
-        }
-
-        this.viewports.delete(event.viewport);
     }
 }
